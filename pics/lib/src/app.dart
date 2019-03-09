8a384760366202d5f7c837e5d2e4a933666efd8f
Vamsi <playstore.vamsi@gmail.com>
2019-03-09 01:03:52 -0500

    State and StatefulWidget

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter clicks'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('Hi there!');
+            setState(() {
+              counter += 1;
+            });
           },
         ),
         appBar: AppBar(
